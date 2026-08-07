c258dd4c16bee545d5c297066e1105f493fc8eda
Eugene G. Tobias <imnoteugene09@gmail.com>
2026-08-08 07:47:48 +0800

    Small fixed

--- app/Services/SerialService.php
+++ app/Services/SerialService.php
@@ -7,105 +7,120 @@ use App\Models\Serial;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class SerialService
 {
-    public function create(array $data): Serial
-    {
-        $serial = DB::transaction(function () use ($data){
+public function create(array $data): Serial
+{
+    $serial = DB::transaction(function () use ($data){
+        $this->saveElectronicFile($data);
 
-            $item = Item::create(
-                Arr::only($data, [
-                    'title', 
-                    'subtitle', 
-                    'description', 
-                    'call_number',
-                    'language',
-                    'publication_year',
-                    'keywords',
-                    'electronic_file',
-                    'branch_id'
-                ])
-            );
+        $item = Item::create(
+            Arr::only($data, [
+                'title', 
+                'subtitle', 
+                'description', 
+                'call_number',
+                'language',
+                'publication_year',
+                'keywords',
+                'electronic_file',
+                'branch_id'
+            ])
+        );
 
-            return $item->serial()->create(
-                arr::only($data, [
-                    'isbn_issn',
-                    'volume',
-                    'issue',
-                    'pages',
-                    'doi',
-                    'item_id'
-                ])
-            );
-            
-        });
-        CacheService::invalidate(CacheService::SERIALS);
+        return $item->serial()->create(
+            arr::only($data, [
+                'isbn_issn',
+                'volume',
+                'issue',
+                'pages',
+                'doi',
+                'item_id'
+            ])
+        );
+        
+    });
+    CacheService::invalidate(CacheService::SERIALS);
 
-        return $serial;
-    }
+    return $serial->load('item');
+}
 
-    public function update(Serial $serial, array $data): Serial
-    {
-        $serial = DB::transaction(function () use ($serial, $data){
+public function update(Serial $serial, array $data): Serial
+{
+    $oldFile = $serial->item->electronic_file;
 
-            $serial->item->update(
-                Arr::only($data, [
-                    'title', 
-                    'subtitle', 
-                    'description', 
-                    'call_number',
-                    'language',
-                    'publication_year',
-                    'keywords',
-                    'electronic_file',
-                    'branch_id'
-                ])
-            );
+    $serial = DB::transaction(function () use ($serial, $data){
 
-            return $serial->update(
-                arr::only($data, [
-                    'isbn_issn',
-                    'volume',
-                    'issue',
-                    'pages',
-                    'doi',
-                    'item_id'
-                ])
-            );
-
-            return $serial->refresh();
-        });
-        CacheService::invalidate(CacheService::SERIALS);
-        return $serial;
-    }
-
-    Public function delete(Serial $serial): bool
-    {
-        $deleted = DB::transaction(function () use ($serial){
-            $serial->item->delete();
-            $serial->delete();
-        });
-
-        if($deleted){
-            CacheService::invalidate(CacheService::ACADEMICS);
+        if (!$this->saveElectronicFile($data)) {
+            unset($data['electronic_file']);
         }
+              
+        $serial->item->update(
+            Arr::only($data, [
+                'title', 
+                'subtitle', 
+                'description', 
+                'call_number',
+                'language',
+                'publication_year',
+                'keywords',
+                'electronic_file',
+                'branch_id'
+            ])
+        );
 
-        return $deleted;
+        return $serial->update(
+            arr::only($data, [
+                'isbn_issn',
+                'volume',
+                'issue',
+                'pages',
+                'doi',
+                'item_id'
+            ])
+        );
+
+        return $serial->fresh(['item']);
+    });
+    if (
+        isset($data['electronic_file']) &&
+        $oldFile &&
+        $oldFile !== $data['electronic_file']
+    ) {
+        Storage::disk('public')->delete($oldFile);
+    }
+    CacheService::invalidate(CacheService::SERIALS);
+    return $serial;
+}
+
+Public function delete(Serial $serial): bool
+{
+    $deleted = DB::transaction(function () use ($serial){
+        $serial->item->delete();
+        $serial->delete();
+    });
+
+    if($deleted){
+        CacheService::invalidate(CacheService::ACADEMICS);
     }
 
-    private function saveElectronicFile(array &$data): bool
-    {
-        if (
-            isset($data['electronic_file']) &&
-            $data['electronic_file'] instanceof UploadedFile
-        ) {
-            $data['electronic_file'] = $data['electronic_file']
-                ->store('item/serials', 'public');
+    return $deleted;
+}
 
-            return true;
-        }
+private function saveElectronicFile(array &$data): bool
+{
+    if (
+        isset($data['electronic_file']) &&
+        $data['electronic_file'] instanceof UploadedFile
+    ) {
+        $data['electronic_file'] = $data['electronic_file']
+            ->store('item/serials', 'public');
 
-        return false;
+        return true;
     }
+
+    return false;
+}
 }
